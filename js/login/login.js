0caf2f0a2cb9aa1ec3c17abf3881e10179407eea
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-01-10 18:46:12 -0500

    Arreglos - Se cambió nuevamente la ruta

--- js/login/login.js
+++ js/login/login.js
@@ -5,22 +5,22 @@ $(document).ready(function(){
 
         $.ajax({
             type:"POST",
-            url:"../../PHP/logica/iniciarSesion.php",
+            url:"PHP/logica/iniciarSesion.php",
             data:cadena,
             success:function(res){
 
                 switch(res){
                     case 1:
-                        window.location="/pages/administrador/indexadmin.html";
+                        window.location="pages/administrador/indexadmin.html";
                         break;
                     case 2:
-                        window.location="/pages/decano/indexadmin.html";
+                        window.location="pages/decano/indexdec.html";
                         break;
                     case 3:
-                        window.location="/pages/jefearea/indexadmin.html";
+                        window.location="pages/jefearea/indexja.html";
                         break;
                     case 4:
-                        window.location="/pages/docentes/indexadmin.html";
+                        window.location="pages/docentes/indexdoc.html";
                         break;
                     default:
                         alertify.alert('Lo lamentamos, pero su usuario no se encuentra en el sistema.');
