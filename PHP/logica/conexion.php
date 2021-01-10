8c44b1797490361b86931be28cea916cab2a886a
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-01-10 15:56:24 -0500

    Configuración de parámetros de BD

--- PHP/logica/conexion.php
+++ PHP/logica/conexion.php
@@ -1,13 +1,22 @@
 <?php
     function conexion(){
-        $servidor="localhost";
-        $usuario="root";
-        $bd="grupocob_sigem";
-        $password="";
+        $servidor="host25.latinoamericahosting.com";
+        $usuario="grupocob_andrefe";
+        $bd="grupocob_SIGEM";
+        $password="Th3Unkindled";
 
         $conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 
-        $this->conexion->set_charset("utf8");
+        $this->$conexion->set_charset("utf8");
+
+        if($conexion)
+        {
+            echo "Conectado a la BD";
+        }
+        else
+        {
+            echo "No se pudo conectar";
+        }
 
         if (mysqli_connect_errno()) {
             print("error al conectarse");
