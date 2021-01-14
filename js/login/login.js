9ddb31d13157d455dbb5884f71ae328c197f8e4b
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-01-13 22:43:04 -0500

    Fragmento de edicion asignatura y login

--- js/login/login.js
+++ js/login/login.js
@@ -8,22 +8,22 @@ $(document).ready(function(){
 
         $.ajax({
             type:"POST",
-            url:"../../PHP/logica/iniciarSesion.php",
+            url:"PHP/logica/iniciarSesion.php",
             data:cadena,
             success:function(res){
 
                 switch(res){
                     case 1:
-                        window.location="./pages/administrador/indexadmin.html";
+                        window.location="/pages/administrador/indexadmin.html";
                         break;
                     case 2:
-                        window.location="./pages/decano/indexdec.html";
+                        window.location="/pages/decano/indexdec.html";
                         break;
                     case 3:
-                        window.location="./pages/jefearea/indexja.html";
+                        window.location="/pages/jefearea/indexja.html";
                         break;
                     case 4:
-                        window.location="./pages/docentes/indexdoc.html";
+                        window.location="/pages/docentes/indexdoc.html";
                         break;
                     default:
                         alertify.alert('Lo lamentamos, pero su usuario no se encuentra en el sistema.');
