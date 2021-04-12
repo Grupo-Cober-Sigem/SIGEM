35fea777262888a0731a72cddbf403208d98a09a
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-04-11 19:19:28 -0500

    Agregar Asignatura

---
+++ js/admin/asignatura/agregarAsignatura.js
@@ -0,0 +1,81 @@
+var url = "";
+var appUsuarios = new Vue({
+    el: "#seccionAsignatura",
+    data: {
+        datosUsuarios: [],
+        cod: "",
+        pass: "",
+        Nombres: "",
+        Apellidos: "",
+        email: "",
+        rol: ""
+    },
+    methods: {
+        btnAgregarAsignatura: async function () {
+            const { value: formValues } = await Swal.fire({
+                title: 'Agregar',
+                html:
+                    '',
+                focusConfirm: false,
+                showCancelButton: true,
+                confirmButtonText: 'Guardar',
+                confirmButtonColor: '#1cc88a',
+                cancelButtonColor: "",
+                cancelButtonColor: '#3085d6',
+
+                preConfirm: () => {
+                    
+                    return [
+                        this.nombre = document.getElementById('Nombre').value,
+                        this.apellidos = document.getElementById('Apellidos').value,
+                        this.Cod_User = document.getElementById('Cod_User').value,
+                        this.email = document.getElementById('email').value,
+                        this.pass = document.getElementById('pass').value,
+                        this.rol = document.getElementById('rol').value
+                    ]
+                }
+            })
+            if (this.nombre == "" || this.apellidos == "" || this.Cod_User == 0 || this.email=='') {
+                Swal.fire({
+                    type: 'info',
+                    title: 'Datos incompletos',
+                })
+            }
+            else {
+                this.agregarUsuarios();
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                Toast.fire({
+                    type: 'success',
+                    title: '¡Usuario Agregado!'
+                })
+            }
+        },
+        btnEditarUser: async function () { },
+        btnDeshabilitarUser: async function () { },
+
+        //Procedimientos
+        listarUsuarios: function(){
+            axios.post(url,{opcion:1}).then(response=>{
+                this.datosUsuarios = response.data;
+                console.log(this.datos);
+            });
+        },
+
+        agregarUsuarios: function(){
+            axios.post(url, {opcion:4, marca:this.marca, modelo:this.modelo,stock:this.stock }).then(response =>{
+                this.listarMoviles();
+            });        
+             this.marca = "",
+             this.modelo = "",
+             this.stock = 0
+        },
+    },
+    created:function(){
+        this.listarUsuarios();
+    }
+});                                 
