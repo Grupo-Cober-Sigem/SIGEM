4912e7bf2854b00b81043f2c3b0ebf5ad53d0461
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-04-15 18:34:37 -0500

    Editar Usuarios 1

--- js/admin/usuarios.js
+++ js/admin/usuarios.js
@@ -14,8 +14,7 @@ var appUsuarios = new Vue({
         btnAgregar: async function () {
             const { value: formValues } = await Swal.fire({
                 title: 'Agregar',
-                html:
-                    '<div class="form-group"><div class="row"><label class="col-form-label"></label></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Nombres</label><div class="col-sm-8"><input id="nombre" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Apellidos</label><div class="col-sm-8"><input id="apellidos" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-6 col-form-label text-left">Nro. de Identificación</label><div class="col-sm-6"><input id="cod_user" type="number" min="0" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-5 col-form-label text-left">Email Unilibre</label><div class="col-sm-7"><input id="email" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Contraseña</label><div class="col-sm-8"><input id="pass" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Rol</label><div class="col-sm-8"><select class="form-control" id="rol"><option value="">Seleccionar</option><option value="1">Docente</option><option value="2">Jefe de Area</option><option value="3">Decano</option></select></div></div></div>',
+                html: '<div class="form-group"><div class="row"><label class="col-form-label"></label></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Nombres</label><div class="col-sm-8"><input id="nombre" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Apellidos</label><div class="col-sm-8"><input id="apellidos" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-6 col-form-label text-left">Nro. de Identificación</label><div class="col-sm-6"><input id="cod_user" type="number" min="0" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-5 col-form-label text-left">Email Unilibre</label><div class="col-sm-7"><input id="email" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Contraseña</label><div class="col-sm-8"><input id="pass" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Rol</label><div class="col-sm-8"><select class="form-control" id="rol"><option value="">Seleccionar</option><option value="1">Docente</option><option value="2">Jefe de Area</option><option value="3">Decano</option></select></div></div></div>',
                 focusConfirm: false,
                 showCancelButton: true,
                 confirmButtonText: 'Guardar',
@@ -32,43 +31,40 @@ var appUsuarios = new Vue({
                         this.email = Swal.getPopup().querySelector('#email').value,
                         this.pass = Swal.getPopup().querySelector('#pass').value,
                         this.rol = Swal.getPopup().querySelector('#rol').value
-                        /*this.nombre = document.getElementById('nombre').value,
-                        this.apellidos = document.getElementById('apellidos').value,
-                        this.cod_user = document.getElementById('cod_user').value,
-                        this.email = document.getElementById('email').value,
-                        this.pass = document.getElementById('pass').value,
-                        this.rol = document.getElementById('rol').value*/
                     ]
                 }
             })
-            console.log("Valor del nombre "+this.nombre);
-            console.log("Valor del apellidos "+this.apellidos);
-            console.log("Valor del cod "+this.cod_user);
-            console.log("Valor del email "+this.email);
-            console.log("Valor del pass "+this.pass);
-            console.log("Valor del rol "+this.rol);
 
-            /*if (this.nombre == "" || this.apellidos == "" || this.cod_user == 0 || this.email=='') {
+            if (this.nombre == "" || this.apellidos == "" || this.cod_user == 0 || this.email==''|| this.pass==''|| this.rol== 0) 
+            {
                 Swal.fire({
                     icon: 'info',
                     title: 'Datos incompletos',
                 })
             }
-            else {*/
+            else 
+            {
                 this.agregarUsuarios();
-                const Toast = Swal.mixin({
+                const Toast = Swal.mixin(
+                {
                     toast: true,
                     position: 'top-end',
                     showConfirmButton: false,
                     timer: 3000
                 });
+
                 Toast.fire({
                     icon: 'success',
                     title: '¡Usuario Agregado!'
                 })
-            /*}*/
+            }
         },
-        btnEditarUser: async function () { },
+        btnEditarUser: async function (cod_user,nombres,apellidos,email,rol) { 
+            axios.post(url, {opcion:2, cod_user:cod_user, nombres:nombres, apellidos:apellidos,email:email,rol:rol }).then(response =>{           
+                this.listarUsuarios();           
+             });    
+        },
+
         btnDeshabilitarUser: async function () { },
 
         //Procedimientos
