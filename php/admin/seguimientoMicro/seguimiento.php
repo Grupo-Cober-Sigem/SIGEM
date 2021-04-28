0f549c692d87d60de35fa18459136116770a95d9
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-04-27 21:06:54 -0500

    Seguimiento. Validación de codigo PHP

--- php/admin/seguimientoMicro/seguimiento.php
+++ php/admin/seguimientoMicro/seguimiento.php
@@ -16,17 +16,16 @@
     $correo = (isset($_POST['email'])) ? $_POST['email'] : '';
     $rol= (isset($_POST['rol'])) ? $_POST['rol'] : '';
 
+    echo $opcion;
+
     switch($opcion){
         case 1: //Listar
             $consulta = "SELECT Cod_Segui, Cod_Asignatura, Cod_User, Horario_Segui, Dia_Segui, Cod_Unidad, Cod_Subte, Actividad_Segui, estrategiaTeams, estrategiaElibre, estrategiaOtro, materialGuias, materialWord, materialDiapositiva, materialVideo, materialOtro, actSincronica_Segui, Participantes_Segui, Observa_Segui, Soporte, Estado, jefeObserva_Segui FROM Seguimiento";
             $resultado = $cn->prepare($consulta);
             $resultado->execute();
             $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
-
-            echo $data
-            break;
-        
-    }
+            break;        
+    };
     // Enviar el array final en formato JSON a Javascript
     print json_encode($data, JSON_UNESCAPED_UNICODE);
     // Cerramos la conexión
