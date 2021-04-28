e79c61b0bc5b881bb71818ba0502293c2ef43e89
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-04-27 20:13:43 -0500

    Seguimiento. Listar seguimientos registrados

--- js/admin/seguimientoMicro/seguimiento.js
+++ js/admin/seguimientoMicro/seguimiento.js
@@ -10,7 +10,7 @@ var appUsuarios = new Vue({
         docenteSeleccionado:"",
         docentes:[],
         //Variables para gestión de registros
-        seguimientos:[],
+        datosSeguimientos:[],
         codSeguimiento:"",
         dia:"",
         hora:"",
@@ -32,52 +32,11 @@ var appUsuarios = new Vue({
     },
     methods: {
         btnAgregarSeguimiento: async function () {
-            const { value: formValues } = await Swal.fire({
-                title: 'Agregar',
-                html: '<div class="form-group"><div class="row"><label class="col-form-label"></label></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Nombres</label><div class="col-sm-8"><input id="nombre" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Apellidos</label><div class="col-sm-8"><input id="apellidos" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-6 col-form-label text-left">Nro. de Identificación</label><div class="col-sm-6"><input id="cod_user" type="number" min="0" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-5 col-form-label text-left">Email Unilibre</label><div class="col-sm-7"><input id="email" type="text" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Contraseña</label><div class="col-sm-8"><input id="pass" type="password" class="form-control"></div></div></div><div class="form-group"><div class="row"><label class="col-sm-4 col-form-label text-left">Rol</label><div class="col-sm-8"><select class="form-control" id="rol"><option value="">Seleccionar</option><option value="1">Docente</option><option value="2">Jefe de Area</option><option value="3">Decano</option></select></div></div></div>',
-                focusConfirm: false,
-                showCancelButton: true,
-                confirmButtonText: 'Guardar',
-                confirmButtonColor: '#1cc88a',
-                showLoaderOnConfirm: true,
-                cancelButtonText: "Cancelar",
-                cancelButtonColor: '#CB3234',
-
-                preConfirm: () => {
-                    return [
-                        this.nombre = Swal.getPopup().querySelector('#nombre').value,
-                        this.apellidos = Swal.getPopup().querySelector('#apellidos').value,
-                        this.cod_user = Swal.getPopup().querySelector('#cod_user').value,
-                        this.email = Swal.getPopup().querySelector('#email').value,
-                        this.pass = Swal.getPopup().querySelector('#pass').value,
-                        this.rol = Swal.getPopup().querySelector('#rol').value
-                    ]
-                }
-            })
-
-            if (this.nombre == "" || this.apellidos == "" || this.cod_user == 0 || this.email==''|| this.pass==''|| this.rol== 0) 
-            {
-                Swal.fire({
-                    icon: 'info',
-                    title: 'Datos incompletos',
-                })
-            }
-            else 
-            {
-                this.agregarUsuarios();
-                const Toast = Swal.mixin(
-                {
-                    toast: true,
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 3000
-                });
-
-                Toast.fire({
-                    icon: 'success',
-                    title: '¡Usuario Agregado!'
-                })
-            }
+            
+            this.dia = document.getElementById("diaSeguimiento").value;
+            this.hora = document.getElementById("horaSeguimiento").value;
+            this.nroEstudiantes = document.getElementById("nroEstudiantes").value;
+            this.actividad = document.getElementById("Actividad").value;
         },
 
         btnEditarSeguimiento: async function (cod_user,nombre,apellidos,email,pass,rol) {
@@ -174,56 +133,16 @@ var appUsuarios = new Vue({
         },
 
         //Procedimientos
-        listarUsuarios: function(){
+        listarSeguimientos: function(){
             axios.post(url,{opcion:1}).then(response=>{
-                this.datosUsuarios = response.data;
-                console.log(this.datosUsuarios);
+
+                this.datosSeguimientos = response.data;
+                console.log(this.datosSeguimientos);
             });
         },
-
-        editarUsuarios: function (cod_user,pass,nombre,apellidos,email,rol){
-            axios.post(url, {opcion:2, cod_user:cod_user, pass:pass, nombre:nombre, apellidos:apellidos,email:email,rol:rol }).then(response =>{           
-                this.listarUsuarios();           
-             });  
-        },
-
-        agregarUsuarios: function(){
-            axios.post(url,{opcion:4, cod_user:this.cod_user, pass:this.pass, nombre:this.nombre, apellidos:this.apellidos, email:this.email, rol:this.rol}).then(response =>{
-                this.listarUsuarios();
-            });        
-            this.cod_user=0,
-            this.pass="",
-            this.nombre="",
-            this.apellidos="",
-            this.email="",
-            this.rol=0
-        },
-
-        deshabilitarUsuarios: function(cod_user,habilitado){
-            if(habilitado=="1")
-            {
-                axios.post(url,{opcion:3, cod_user:cod_user}).then(response =>{           
-                    this.listarUsuarios();
-
-                    Swal.fire(
-                        'Deshabilitado',
-                        'El usuario ya no tiene acceso al sistema',
-                        'success'
-                    )
-                });  
-            }else
-            {
-                Swal.fire(
-                    'Deshabilitado',
-                    'El usuario ya se encuentra deshabilitado',
-                    'info'
-                )
-            }
-            
-        }
     },
 
     created:function(){
-        this.listarUsuarios();
+        this.listarSeguimientos();
     }
 });                                 
--- php/admin/seguimientoMicro/seguimiento.php
+++ php/admin/seguimientoMicro/seguimiento.php
@@ -1,3 +1,32 @@
 <?php
+    include_once '../../conexion.php';
 
+    $conexion = new Conexion();
+    $cn = $conexion->Conectar();
+
+    //Recepcion de parámetros mediante axios
+    $_POST = json_decode(file_get_contents("php://input"), true);
+
+    $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+
+    $usuario = (isset($_POST['cod_user'])) ? $_POST['cod_user'] : '';
+    $pass = (isset($_POST['pass'])) ? $_POST['pass'] : '';
+    $nombres = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
+    $apellidos = (isset($_POST['apellidos'])) ? $_POST['apellidos'] : '';
+    $correo = (isset($_POST['email'])) ? $_POST['email'] : '';
+    $rol= (isset($_POST['rol'])) ? $_POST['rol'] : '';
+
+    switch($opcion){
+        case 1: //Listar
+            $consulta = "SELECT Cod_Segui, Cod_Asignatura, Cod_User, Nombre_Asig, Horario_Segui, Dia_Segui, Cod_Unidad, Cod_Subte, Actividad_Segui, estrategiaTeams, estrategiaElibre, estrategiaOtro, materialGuias, materialWord, materialDiapositiva, materialVideos, materialOtro, actSincronica_Segui, Participantes_Segui, Observa_Segui, Soprte, Estado, jefeObserva_Segui FROM Seguimiento";
+            $resultado = $cn->prepare($consulta);
+            $resultado->execute();
+            $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            break;
+        
+    }
+    // Enviar el array final en formato JSON a Javascript
+    print json_encode($data, JSON_UNESCAPED_UNICODE);
+    // Cerramos la conexión
+    $conexion = NULL;
 ?>
