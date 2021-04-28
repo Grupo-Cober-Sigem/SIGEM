a6802334e48f9945336bdf7a1b86b560953e974a
DonCamilo78 <andresbernal229@gmail.com>
2021-04-27 22:29:07 -0500

    Arreglando boton de cerrar sesión en las pestañas de seguimiento y estadisticas

--- pages/administrador/estadisticas/statistics.js
+++ pages/administrador/estadisticas/statistics.js
@@ -5,21 +5,21 @@ const etiquetas = ["Estado"]
 // Podemos tener varios conjuntos de datos
 const seguimientosaprobados = {
     label: "Seguimientos Aprobados",
-    data: [5000], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    data: [8], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
     borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
     borderWidth: 1,// Ancho del borde
 };
 const seguimientosporrevisar = {
     label: "Seguimientos por Revisar",
-    data: [10000], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    data: [10], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
     borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
     borderWidth: 1,// Ancho del borde
 };
 const seguimientosrechazados = {
     label: "Seguimientos Rechazados",
-    data: [10000], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
     borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
     borderWidth: 1,// Ancho del borde
