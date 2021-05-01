7e0a0f816340ce768be25fe0e5a1c230ffeb7041
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-05-01 16:33:41 -0500

    Seguimiento. Mezcla del front con back para filtrado de asignaturas #2

--- js/admin/seguimientoMicro/seguimiento.js
+++ js/admin/seguimientoMicro/seguimiento.js
@@ -96,6 +96,8 @@ var appUsuarios = new Vue({
             axios.post(url,{opcion:2}).then(response=>{
 
                 this.asignaturas = response.data;
+                console.log("Valor del array Asignatura"+this.asignaturas);
+
                 this.unidadSeleccionada="";
                 this.unidades=[];
                 this.subtemaSeleccionado="";
