43bc848f138eb91928b098d79dfe04c1f31f4ee5
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 16:26:53 -0500

    Probando estadísticas VIII

--- php/admin/estadisticas.php
+++ php/admin/estadisticas.php
@@ -14,7 +14,7 @@
             $consultaaprobado = "SELECT count(Estado) FROM Seguimiento WHERE Estado='aprobado'";
             $resultado = $cn->prepare($consultaaprobado);
             $resultado->execute();
-            $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            $data = $resultado;
             echo $data;
             break;
 
