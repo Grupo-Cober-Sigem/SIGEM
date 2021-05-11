0505894a1f3ccd02f4f3adcdeb95b87637df2746
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-05-10 21:04:49 -0500

    Seguimiento. Editar - Vincular select al nuevo modal #2

--- js/admin/seguimientoMicro/seguimiento.js
+++ js/admin/seguimientoMicro/seguimiento.js
@@ -122,8 +122,10 @@ var appUsuarios = new Vue({
             document.getElementById("diaSeguimientoEdit").value = dia;
             document.getElementById("horaSeguimientoEdit").value = hora;
             document.getElementById("nroEstudiantesEdit").value = participantes;
+            this.traerAsignatura();
             document.getElementById("asignaturaEdit").value = asignatura;
             this.traerUnidades();
+            document.getElementById("unidadEdit").value = unidad;
             this.traerSubtemas();
             document.getElementById("unidadEdit").value = unidad;
             document.getElementById("subtemaEdit").value = subtema;
