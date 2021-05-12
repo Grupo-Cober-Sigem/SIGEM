791ed8933ef2a21b4c0939076100236dca307d0c
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 21:26:51 -0500

    Prueba 52

--- js/admin/estadisticas/statistics.js
+++ js/admin/estadisticas/statistics.js
@@ -35,21 +35,21 @@ var appUsuarios = new Vue({
             // Podemos tener varios conjuntos de datos
             const seguimientosaprobados = {
                 label: "Seguimientos Aprobados",
-                data: [this.aprobado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
                 borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosporrevisar = {
                 label: "Seguimientos por Revisar",
-                data: [this.pendiente], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosrechazados = {
                 label: "Seguimientos Rechazados",
-                data: [this.rechazado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [0], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
