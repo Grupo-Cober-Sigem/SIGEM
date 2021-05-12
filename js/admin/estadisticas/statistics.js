54c255e76227fb56442ac21b7cbbb0c6a30c5b39
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 21:29:23 -0500

    Prueba 53

--- js/admin/estadisticas/statistics.js
+++ js/admin/estadisticas/statistics.js
@@ -1,55 +1,25 @@
-var url = "../../../php/admin/estadisticas.php";
-var appUsuarios = new Vue({
-    el: "#seccionEstadisticas",
-    data: {
-        aprobado: [],
-        pendiente: [],
-        rechazado: []
-    },
-    methods: {
-        //Procedimientos
-        contarAprobados: function(){
-            axios.post(url,{opcion:1}).then(response=>{
-                this.aprobado = response.data;
-                console.log("Este es el valor de aprobado "+this.aprobado);
-            });
-        },
-        contarPendientes: function(){
-            axios.post(url,{opcion:2}).then(response=>{
-                this.pendiente = response.data;
-                console.log("Este es el valor de pendiente "+this.pendiente);
-            });
-        },
-        contarRechazados: function(){
-            axios.post(url,{opcion:3}).then(response=>{
-                this.rechazado = response.data;
-                console.log("Este es el valor de rechazado "+this.rechazado);
-            });
-        },
-        MostrarGrafica: function(){
-
-            // Obtener una referencia al elemento canvas del DOM
+// Obtener una referencia al elemento canvas del DOM
             const $grafica = document.querySelector("#grafica");
             // Las etiquetas son las que van en el eje X.
             const etiquetas = ["Estado"]
             // Podemos tener varios conjuntos de datos
             const seguimientosaprobados = {
                 label: "Seguimientos Aprobados",
-                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [this.aprobado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
                 borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosporrevisar = {
                 label: "Seguimientos por Revisar",
-                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [10], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosrechazados = {
                 label: "Seguimientos Rechazados",
-                data: [0], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
@@ -76,15 +46,3 @@ var appUsuarios = new Vue({
                     },
                 }
             });
-        },
-
-
-    },
-
-    created:function(){
-        this.contarAprobados();
-        this.contarPendientes();
-        this.contarRechazados();
-        this.MostrarGrafica();
-    }
-});
