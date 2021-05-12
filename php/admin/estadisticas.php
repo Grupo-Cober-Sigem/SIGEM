31ab0433cd7487f00cc0277d381fd144528d9aed
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 21:15:33 -0500

    Prueba 48

--- php/admin/estadisticas.php
+++ php/admin/estadisticas.php
@@ -1,49 +1,43 @@
 <?php
 include_once '../conexion.php';
 
-
-
 $conexion = new Conexion();
 $cn = $conexion->Conectar();
-
-
 
 //Recepcion de parámetros mediante axios
 $_POST = json_decode(file_get_contents("php://input"), true);
 
-
-
 $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
 
-
-
 switch($opcion){
-case 1: //Aprobado
-$consultaaprobado = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='aprobado'";
-$resultado = $cn->prepare($consultaaprobado);
-$resultado->execute();
-$data = $resultado->fetch(PDO::FETCH_ASSOC);
-echo "Prueba con Ing {$data}";
-break;
+    case 1: //Aprobado
+        $consultaaprobado = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='aprobado'";
+        $resultado = $cn->prepare($consultaaprobado);
+        $resultado->execute();
+        $data = $resultado->fetch(PDO::FETCH_ASSOC);
+        $dato = $data["prueba"];
+        echo $dato;
+        break;
 
+    case 2: //Pendiente
+        $consultapendiente = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='pendiente'";
+        $resultado = $cn->prepare($consultapendiente);
+        $resultado->execute();
+        $data = $resultado->fetch(PDO::FETCH_ASSOC);
+        $dato = $data["prueba"];
+        echo "Prueba con Ing {$dato}";
+        break;
 
-
-case 2: //Pendiente
-$consultapendiente = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='pendiente'";
-$resultado = $cn->prepare($consultapendiente);
-$resultado->execute();
-$data = $resultado->fetch(PDO::FETCH_ASSOC);
-break;
-
-
-
-case 3: //Rechazar
-$consultarechazado = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='rechazado'";
-$resultado = $cn->prepare($consultarechazado);
-$resultado->execute();
-$data = $resultado->fetch(PDO::FETCH_ASSOC);
-break;
-};
+    case 3: //Rechazar
+        $consultarechazado = "SELECT count(Estado) as prueba FROM Seguimiento WHERE Estado='rechazado'";
+        $resultado = $cn->prepare($consultarechazado);
+        $resultado->execute();
+        $data = $resultado->fetch(PDO::FETCH_ASSOC);
+        //$row = mysqli_fetch_array($resultado, MYSQLI_ASSOC);
+        $dato = $data["prueba"];
+        echo $dato;
+        break;
+    };
 
 
 
