0767b4a49d0b85f89df86bc5f1aac56d9b1d47fe
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-05-12 01:01:33 -0500

    Estadisticas. Prueba 96 - Consultas axios anidadas junto al llamado de la gráfica

--- js/admin/estadisticas/statistics.js
+++ js/admin/estadisticas/statistics.js
@@ -1,9 +1,4 @@
 var url = "../../../php/admin/estadisticas.php";
-
-var ap = 5;
-var pe = 0;
-var re = 0;
-
 var appUsuarios = new Vue({
     data: {
         aprobado: [],
@@ -32,73 +27,75 @@ var appUsuarios = new Vue({
                 console.log("Este es el valor de rechazado "+this.rechazado);
             });
         },
+
         mostrarGrafica: function(){
-
-            console.log(this.aprobado);
-            //var a = Number(this.aprobado);
-            //console.log(a);
-
             // Obtener una referencia al elemento canvas del DOM
             const $grafica = document.querySelector("#grafica");
             // Las etiquetas son las que van en el eje X.
             const etiquetas = ["Estado"]
             // Podemos tener varios conjuntos de datos
-            ap = 4;
-            axios.post(url,{opcion:1}).then(response=>{
+
+            axios.post(url,{opcion:1}).then(response=>{ //Inicia APROBADOS
                 this.aprobado = response.data;
                 console.log("Este es el valor de aprobado "+this.aprobado);
-                ap = this.aprobado;
-                console.log(ap);
+                
+                axios.post(url,{opcion:2}).then(response=>{ //Inicia PENDIENTE
+                    this.pendiente = response.data;
+                    console.log("Este es el valor de pendiente "+this.pendiente);
 
-                const seguimientosaprobados = {
-                label: "Seguimientos Aprobados",
-                data: [ap], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
-                borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
-            const seguimientosporrevisar = {
-                label: "Seguimientos por Revisar",
-                data: [10], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
-                borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
-            const seguimientosrechazados = {
-                label: "Seguimientos Rechazados",
-                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
-                borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
+                    axios.post(url,{opcion:3}).then(response=>{ //Inicia RECHAZADO
+                        this.rechazado = response.data;
+                        console.log("Este es el valor de rechazado "+this.rechazado);
 
-            new Chart($grafica, {
-                type: 'bar',// Tipo de gráfica
-                data: {
-                    labels: etiquetas,
-                    datasets: [
-                        seguimientosaprobados,
-                        seguimientosporrevisar,
-                        seguimientosrechazados,
-                        // Aquí más datos...
-                    ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
+                        const seguimientosaprobados = {
+                            label: "Seguimientos Aprobados",
+                            data: [this.aprobado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                            backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
+                            borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
+                            borderWidth: 1,// Ancho del borde
+                        };
+                        const seguimientosporrevisar = {
+                            label: "Seguimientos por Revisar",
+                            data: [this.pendiente], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                            backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
+                            borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
+                            borderWidth: 1,// Ancho del borde
+                        };
+                        const seguimientosrechazados = {
+                            label: "Seguimientos Rechazados",
+                            data: [this.rechazado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                            backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
+                            borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
+                            borderWidth: 1,// Ancho del borde
+                        };
+            
+                        new Chart($grafica, {
+                            type: 'bar',// Tipo de gráfica
+                            data: {
+                                labels: etiquetas,
+                                datasets: [
+                                    seguimientosaprobados,
+                                    seguimientosporrevisar,
+                                    seguimientosrechazados,
+                                    // Aquí más datos...
+                                ]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }],
+                                },
                             }
-                        }],
-                    },
-                }
-            });
-            });
+                        });
 
-
+                    }); //Termina RECHAZADO
+                }); //Termina PENDIENTES
+            }); //Termina APROBADOS
         }
-    },/*
-    */
+    },
     created:function(){
         this.contarAprobados();
         this.contarPendientes();
