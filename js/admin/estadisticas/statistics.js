9b1ac0142f81e2b754ced31b1cf8cff6132145eb
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 22:51:17 -0500

    Prueba 83

--- js/admin/estadisticas/statistics.js
+++ js/admin/estadisticas/statistics.js
@@ -16,26 +16,20 @@ var appUsuarios = new Vue({
             axios.post(url,{opcion:1}).then(response=>{
                 this.aprobado = response.data;
                 console.log("Este es el valor de aprobado "+this.aprobado);
-                ap = this.aprobado;
-                console.log(ap);
-            });
-        },
-        contarPendientes: function(){
+            }),
+
             axios.post(url,{opcion:2}).then(response=>{
                 this.pendiente = response.data;
                 console.log("Este es el valor de pendiente "+this.pendiente);
-            });
-        },
-        contarRechazados: function(){
+            }),
+
             axios.post(url,{opcion:3}).then(response=>{
                 this.rechazado = response.data;
                 console.log("Este es el valor de rechazado "+this.rechazado);
-            });
-        },
-        mostrarGrafica: function(){
+            }),
 
             console.log(this.aprobado);
-            //var a = Number(this.aprobado);
+            var apr = Number(this.aprobado);
             //console.log(a);
 
             // Obtener una referencia al elemento canvas del DOM
@@ -45,7 +39,7 @@ var appUsuarios = new Vue({
             // Podemos tener varios conjuntos de datos
             const seguimientosaprobados = {
                 label: "Seguimientos Aprobados",
-                data: [ap], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [apr], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
                 borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
                 borderWidth: 1,// Ancho del borde
@@ -96,52 +90,3 @@ var appUsuarios = new Vue({
         this.mostrarGrafica();
     }/*
 */});
-
-            // Obtener una referencia al elemento canvas del DOM
-            const $grafica = document.querySelector("#grafica");
-            // Las etiquetas son las que van en el eje X.
-            const etiquetas = ["Estado"]
-            // Podemos tener varios conjuntos de datos
-            const seguimientosaprobados = {
-                label: "Seguimientos Aprobados",
-                data: [ap], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
-                borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
-            const seguimientosporrevisar = {
-                label: "Seguimientos por Revisar",
-                data: [10], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
-                borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
-            const seguimientosrechazados = {
-                label: "Seguimientos Rechazados",
-                data: [2], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-                backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
-                borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
-                borderWidth: 1,// Ancho del borde
-            };
-
-            new Chart($grafica, {
-                type: 'bar',// Tipo de gráfica
-                data: {
-                    labels: etiquetas,
-                    datasets: [
-                        seguimientosaprobados,
-                        seguimientosporrevisar,
-                        seguimientosrechazados,
-                        // Aquí más datos...
-                    ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }],
-                    },
-                }
-            });
