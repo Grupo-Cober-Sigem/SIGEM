39181763a3f6faccc8b3658ee10e04753ed58671
DonCamilo78 <andresbernal229@gmail.com>
2021-05-11 20:42:56 -0500

    PRUEBA 40

--- js/admin/estadisticas/statistics.js
+++ js/admin/estadisticas/statistics.js
@@ -2,48 +2,32 @@ var url = "../../../php/admin/estadisticas.php";
 var appUsuarios = new Vue({
     el: "#seccionEstadisticas",
     data: {
-        aprobado: [],
-        pendiente: [],
-        rechazado: []
+        aprobado: 0,
+        pendiente: 0,
+        rechazado: 0
     },
     methods: {
         //Procedimientos
         contarAprobados: function(){
             axios.post(url,{opcion:1}).then(response=>{
-                console.log("Este es el valor de aprobado "+response.data);
-                this.aprobado = response.data;
+                this.aprobado = response.dato;
                 console.log("Este es el valor de aprobado "+this.aprobado);
             });
         },
         contarPendientes: function(){
             axios.post(url,{opcion:2}).then(response=>{
-                this.pendiente = response.data;
+                this.pendiente = response.dato;
                 console.log("Este es el valor de pendiente "+this.pendiente);
             });
         },
         contarRechazados: function(){
             axios.post(url,{opcion:3}).then(response=>{
-                this.rechazado = response.data;
+                this.rechazado = response.dato;
                 console.log("Este es el valor de rechazado "+this.rechazado);
             });
         },
         MostrarGrafica: function(){
 
-            var nAprobado =0; 
-            
-            this.aprobado.map(num=>
-                nAprobado=num.prueba
-            );
-
-            var nPendiente = this.pendiente.map(function(num){
-                return num;
-            });
-
-            var nRechazado = this.rechazado.map(function(num){
-                return num;
-            });
-
-            console.log("Valor de nAprobado")
             // Obtener una referencia al elemento canvas del DOM
             const $grafica = document.querySelector("#grafica");
             // Las etiquetas son las que van en el eje X.
@@ -51,21 +35,21 @@ var appUsuarios = new Vue({
             // Podemos tener varios conjuntos de datos
             const seguimientosaprobados = {
                 label: "Seguimientos Aprobados",
-                data: [nAprobado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [this.aprobado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(0, 143, 57, 0.2)', // Color de fondo
                 borderColor: 'rgba(0, 143, 57, 1)', // Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosporrevisar = {
                 label: "Seguimientos por Revisar",
-                data: [nPendiente], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [this.pendiente], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 128, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 128, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
             };
             const seguimientosrechazados = {
                 label: "Seguimientos Rechazados",
-                data: [nRechazado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: [this.rechazado], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 backgroundColor: 'rgba(255, 0, 0, 0.2)',// Color de fondo
                 borderColor: 'rgba(255, 0, 0, 1)',// Color del borde
                 borderWidth: 1,// Ancho del borde
