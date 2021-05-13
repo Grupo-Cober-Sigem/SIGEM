338bca716af6c4f4786dd13ff4cab08f5356d5fb
DonCamilo78 <andresbernal229@gmail.com>
2021-05-13 02:28:54 -0500

    Modificación en REVISIÓN de los roles JEFE DE AREA y DECANO

---
+++ js/decano/seguimientoMicro/seguimientoRevisionxdec.js
@@ -0,0 +1,110 @@
+var url = "../../../php/decano/seguimientoMicro/seguimientoRevisionxdec.php";
+var appUsuarios = new Vue({
+    el: "#revisarSeguimiento",
+    data: {
+        //Variables para gestión de registros
+        datosSeguimientos:[]
+    },
+    methods: {
+
+        aprobarRegistro: async function(codSeguimiento){
+            axios.post(url,{opcion:2, codigoSegui:codSeguimiento}).then(response=>{
+
+                this.listarSeguimientos();
+                    Swal.fire(
+                        '¡Registro exitoso!',
+                        'El registro ha sido aprobado con éxito.',
+                        'success'
+                      )
+            });
+        },
+
+        cargarObservacion: async function(codSeguimiento){
+            localStorage.setItem("codSeguimiento",codSeguimiento);
+        },
+
+        rechazarRegistro: async function(){
+
+            var observacionJefe = document.getElementById("observacionesJefe").value;
+            var codSeguimiento = localStorage.getItem("codSeguimiento");
+
+            if(observacionJefe=="" || observacionJefe==null){
+
+                localStorage.removeItem("codSeguimiento");
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Por favor digite una observación.',
+                })
+            }else{
+
+                axios.post(url,{opcion:3,observacionJefe:observacionJefe,codigoSegui:codSeguimiento}).then(response=>{
+
+                    localStorage.removeItem("codSeguimiento");
+                    this.limpiarModal();
+                    this.listarSeguimientos();
+
+                    Swal.fire(
+                        '¡Registro exitoso!',
+                        'El registro ha sido rechazado con éxito.',
+                        'success'
+                      )
+                });
+            }
+        },
+
+        //Procedimientos
+        listarSeguimientos: function(){
+            axios.post(url,{opcion:1}).then(response=>{
+
+                this.datosSeguimientos = response.data;
+                this.datosSeguimientos.map(function(seguimiento){
+
+                    if(seguimiento.estrategiaTeams=="1"){
+                        seguimiento.estrategiaTeams="Microsoft Teams.";
+                    }else{
+                        seguimiento.estrategiaTeams="";
+                    }
+
+                    if(seguimiento.estrategiaElibre=="1"){
+                        seguimiento.estrategiaElibre="Plataforma Elibre.";
+                    }else{
+                        seguimiento.estrategiaElibre="";
+                    }
+
+                    if(seguimiento.materialGuias=="1"){
+                        seguimiento.materialGuias="Guia de aprendizaje.";
+                    }else{
+                        seguimiento.materialGuias="";
+                    }
+
+                    if(seguimiento.materialWord=="1"){
+                        seguimiento.materialWord="Instructivo en excel o word.";
+                    }else{
+                        seguimiento.materialWord="";
+                    }
+
+                    if(seguimiento.materialDiapositiva=="1"){
+                        seguimiento.materialDiapositiva="Presentación diapositivas.";
+                    }else{
+                        seguimiento.materialDiapositiva="";
+                    }
+
+                    if(seguimiento.materialVideos=="1"){
+                        seguimiento.materialVideos="Videos.";
+                    }else{
+                        seguimiento.materialVideos="";
+                    }
+                });
+            });
+        },
+
+        limpiarModal: function(){
+
+            document.getElementById("observacionesJefe").value="";
+        },
+    },
+
+    created:function(){
+        this.listarSeguimientos();
+    }
+});
---
+++ php/decano/seguimientoMicro/seguimientoRevisionxdec.php
@@ -0,0 +1,39 @@
+<?php
+    include_once '../../conexion.php';
+
+    $conexion = new Conexion();
+    $cn = $conexion->Conectar();
+
+    //Recepcion de parámetros mediante axios
+    $_POST = json_decode(file_get_contents("php://input"), true);
+
+    $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+
+    $codigoSegui = (isset($_POST['codigoSegui'])) ? $_POST['codigoSegui'] : '';
+    $observacionJefe = (isset($_POST['observacionJefe'])) ? $_POST['observacionJefe'] : '';
+
+    switch($opcion){
+        case 1: //Listar
+            $consulta = "SELECT S.Cod_Segui, S.Horario_Segui, S.Dia_Segui, U.Nombre_Unidad, T.Nombre_Subte, S.Actividad_Segui, S.estrategiaTeams, S.estrategiaElibre, S.estrategiaOtro, S.materialGuias, S.materialWord, S.materialDiapositiva, S.materialVideo, S.materialOtro, S.actSincronica_Segui, S.Participantes_Segui, S.Observa_Segui, S.Soporte, S.Estado FROM Seguimiento S INNER JOIN Unidades U ON S.Cod_Unidad = U.Cod_Unidad INNER JOIN Subtemas T ON S.Cod_Subte = T.Cod_Subte WHERE S.Estado='Pendiente'";
+            $resultado = $cn->prepare($consulta);
+            $resultado->execute();
+            $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            break;
+
+        case 2: //Aprobar registros
+            $consulta = "UPDATE Seguimiento SET Estado='Aprobado' WHERE Cod_Segui='$codigoSegui'";
+            $resultado = $cn->prepare($consulta);
+            $resultado->execute();
+            break;
+
+        case 3: //Rechazar registro
+            $consulta = "UPDATE Seguimiento SET jefeObserva_Segui='$observacionJefe', Estado='Rechazado' WHERE Cod_Segui='$codigoSegui'";
+            $resultado = $cn->prepare($consulta);
+            $resultado->execute();
+            break;
+    };
+    // Enviar el array final en formato JSON a Javascript
+    print json_encode($data, JSON_UNESCAPED_UNICODE);
+    // Cerramos la conexión
+    $conexion = NULL;
+?>
