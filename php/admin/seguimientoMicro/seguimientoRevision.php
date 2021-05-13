8250f2b6e6a7d7f0b9ff329103af46c33253efdd
AndreFe98 <71802844+AndreFe98@users.noreply.github.com>
2021-05-12 22:38:37 -0500

    Seguimiento. Revision - Listar seguimientos pendientes #4

--- php/admin/seguimientoMicro/seguimientoRevision.php
+++ php/admin/seguimientoMicro/seguimientoRevision.php
@@ -11,7 +11,7 @@
 
     switch($opcion){
         case 1: //Listar
-            $consulta = "SELECT S.Cod_Segui, S.Horario_Segui, S.Dia_Segui, U.Nombre_Unidad, T.Nombre_Subte, S.Actividad_Segui, S.estrategiaTeams, S.estrategiaElibre, S.estrategiaOtro, S.materialGuias, S.materialWord, S.materialDiapositiva, S.materialVideo, S.materialOtro, S.actSincronica_Segui, S.Participantes_Segui, S.Observa_Segui, S.Soporte FROM Seguimiento S INNER JOIN Unidades U ON S.Cod_Unidad = U.Cod_Unidad INNER JOIN Subtemas T ON S.Cod_Subte = T.Cod_Subte WHERE S.Estado ='Pendiente'";
+            $consulta = "SELECT S.Cod_Segui, S.Horario_Segui, S.Dia_Segui, U.Nombre_Unidad, T.Nombre_Subte, S.Actividad_Segui, S.estrategiaTeams, S.estrategiaElibre, S.estrategiaOtro, S.materialGuias, S.materialWord, S.materialDiapositiva, S.materialVideo, S.materialOtro, S.actSincronica_Segui, S.Participantes_Segui, S.Observa_Segui, S.Soporte FROM Seguimiento S INNER JOIN Unidades U ON S.Cod_Unidad = U.Cod_Unidad INNER JOIN Subtemas T ON S.Cod_Subte = T.Cod_Subte WHERE S.Estado='Pendiente'";
             $resultado = $cn->prepare($consulta);
             $resultado->execute();
             $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
