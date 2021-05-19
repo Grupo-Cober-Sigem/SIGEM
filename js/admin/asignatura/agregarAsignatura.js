0269432fec431616e7cc1690b1b1182f755d05a3
DonCamilo78 <andresbernal229@gmail.com>
2021-05-19 14:22:27 -0500

    PRUEBA 5 - editar asignaturas rol ADMIN

--- js/admin/asignatura/agregarAsignatura.js
+++ js/admin/asignatura/agregarAsignatura.js
@@ -233,7 +233,7 @@ var appAsignatura = new Vue({
 
         editarAsignatura:function(codigo, nombre, nivelFormacion, area, docente){
             axios.post(url,{opcion:7, nombre:nombre, codigo:codigo, nivelFormacion:nivelFormacion, area:area, docente:docente}).then(response => {
-                this.listarAsignatura();
+                this.listarAsignaturas();
                 Swal.fire(
                     '¡Actualizado!',
                     'El registro ha sido editado con éxito.',
